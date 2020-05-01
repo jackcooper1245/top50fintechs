b7ffd69c5255238b6c6b294f5f927cfce41d2754
Jack Cooper <jackcooper1245@gmail.com>
2020-05-01 10:46:05 +0000

    Initialie git

---
+++ lib/top50fintechs/cli.rb
@@ -0,0 +1,12 @@
+class Top50fintechs::CLI
+
+def call
+  puts "Welcome to Top50FinTechs."
+  puts "Please choose from the categories below."
+  puts "To select a category please enter the corresponding index number."
+  puts "1. Top 50 Fintech companies of 2019"
+  puts "2. Top 10 Fintech companies of 2019"
+  puts "3. Fintech Hall of Fame of the 2010s"
+end
+
+end
