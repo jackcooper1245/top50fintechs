12620a44f186c8b9400b4d7c27fa136bff023bff
Jack Cooper <jackcooper1245@gmail.com>
2020-05-01 12:02:58 +0000

    Initialize git

--- lib/top50fintechs/companies.rb
+++ lib/top50fintechs/companies.rb
@@ -1,5 +1,7 @@
 class Top50fintechs::Company
 
+attr_accessor :company_name, :website, :twitter, :linkedin, :founders, :founded, :who_is_it_for, :keywords, :latest_funding, :total_funding, :offices, :company_moto, :bio
+
 #The company needs to be created with:
 # A company name, website, twitter, linkedin
 # Founders, when founded, Who is it for?,
