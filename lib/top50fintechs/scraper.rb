b4847e44712cb526e635e2365e4900e188fe3ffb
Jack Cooper <jackcooper1245@gmail.com>
2020-05-01 13:51:47 +0000

    Initialize git

--- lib/top50fintechs/scraper.rb
+++ lib/top50fintechs/scraper.rb
@@ -1,3 +1,4 @@
+require 'rubygems'
 require 'pry'
 require 'open-uri'
 require 'nokogiri'
@@ -5,26 +6,22 @@ require 'nokogiri'
 class Top50fintechs::Scraper
 
 def get_page_top_50_2019
-  html = open('https://thefintech50.com/the-fintech50-2019-50-fintechs-to-watch-in-2019')
-  Nokogiri::HTML(html)
+   Nokogiri::HTML(open('https://thefintech50.com/the-fintech50-2019-50-fintechs-to-watch-in-2019'))
 end
 
 def get_page_top_10_2019
-  html = open('https://thefintech50.com/the-hot-ten-2019')
-  Nokogiri::HTML(html)
+  Nokogiri::HTML(open('https://thefintech50.com/the-hot-ten-2019'))
 end
 
 def get_page_hall_of_fame
-  html = open('https://thefintech50.com/the-fintech50-hall-of-fame-1')
-  Nokogiri::HTML(html)
+  Nokogiri::HTML(open('https://thefintech50.com/the-fintech50-hall-of-fame-1'))
 end
 
-def scrape_companies_list(index_url_method)
-company_list = self.index_url_method.css('div.sqs-gallery sqs-galler-design-grid')
-company_list.each_with_index do |company, index|
- puts "#{index + 1}. #{company.css('a').attribute('href').value}"
- binding.pry
-end
-end
+#def scrape_companies_list(index_url_method)
+#company_list = self.index_url_method.css('div.sqs-gallery sqs-galler-design-grid')
+#company_list.each_with_index do |company, index|
+# puts "#{index + 1}. #{company.css('a').attribute('href').value}"
+#end
+#end
 
 end
