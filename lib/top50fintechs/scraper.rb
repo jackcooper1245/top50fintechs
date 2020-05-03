8047e909ff06cbf56c0755a535ed77ee4ff7f6cd
Jack Cooper <jackcooper1245@gmail.com>
2020-05-03 10:02:19 +0000

    Initialize git

--- lib/top50fintechs/scraper.rb
+++ lib/top50fintechs/scraper.rb
@@ -16,45 +16,33 @@ end
 
 
 def scrape
-  scrape_details
-  scrape_companies_titles
-  @category
+  scrape_companies_titles_and_urls
 end
 
 def scrape_details
   @category.title = @doc.search('h2')[2]
 end
 
-def scrape_companies_titles
-  @doc.search('div[class=margin-wrapper]').each do |company|
-    c = Company.new
-    c.name = company.search('a').attribute('href').value.split('/').join.gsub("-", " ").capitalize
-    @category.add_companies(c)
-  end
-  @category.companies
-end
-
-def scrape_company_urls
-  @doc.search('div[class=margin-wrapper]').each do |company|
-  @company_urls << company_url = "https://thefintech50.com#{company.search('a').attr('href').text}"
-  end
-  @company_urls
-end
-
-def scrape_company_profiles
+def scrape_companies_titles_and_urls
   company_array = []
-  @company_urls.each do |url|
-  profile = Nokogiri::HTML(open(url))
-    names = profile.css('h2')[0].text
-  company_array << names
-end
-company_array
+  @doc.search('div[class=margin-wrapper]').each do |company|
+    company_hash = {
+    :name => company.search('a').attribute('href').value.split('/').join.gsub("-", " ").capitalize,
+    :company_url => "https://thefintech50.com#{company.search('a').attr('href').text}"}
+    company_array << company_hash
+  end
+  company_array
 end
 
-def get_test
-html = Nokogiri::HTML(open(@company_urls[0]))
-test1 = html.css('h2')[0].text
-test1
-end
+
+#def scrape_company_profiles
+#  company_array = []
+#  @company_urls.each do |url|
+#  profile = Nokogiri::HTML(open(url))
+#   names = profile.css('h2')[0].text
+#  company_array << names
+#end
+#company_array
+#end
 
 end
